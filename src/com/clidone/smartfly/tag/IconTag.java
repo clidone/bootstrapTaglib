9c3d3efebfa2c2a72818b5f397b0f93d8bf88524
wuhuaxia <wuhuaxia@192.168.2.103>
2017-04-11 15:26:52 +0800

    add bootstrap tag

---
+++ src/com/clidone/smartfly/tag/IconTag.java
@@ -0,0 +1,11 @@
+package com.clidone.smartfly.tag;
+
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ * <strong>图标标签对象</strong>
+ * @author wuhuaxia
+ */
+public final class IconTag extends TagSupport {
+
+}
