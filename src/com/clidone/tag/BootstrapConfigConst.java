8cc8599f2d7b095147eed4dc220be2b74c3e7f60
huaxia0624 <huaxia0624@126.com>
2017-08-16 01:50:36 +0800

    Add config constant file

---
+++ src/com/clidone/tag/BootstrapConfigConst.java
@@ -0,0 +1,26 @@
+package com.clidone.tag;
+
+/**
+ * <strong>Bootstrap taglib constant class</strong>
+ * @author wuhuaxia
+ */
+public class BootstrapConfigConst {
+
+    /**
+     * Define taglib using version
+     */
+    public static final String BOOTSTRAP_TAGLIB_VERSION_KEY = "bootstrap.taglib.version";
+
+    /**
+     * Define taglib using icon prefix
+     */
+    public static final String BOOTSTRAP_TAGLIB_ICON_KEY = "bootstrap.taglib.icon";
+
+    /**
+     * Constructor
+     * @throws Exception
+     */
+    public BootstrapConfigConst() throws Exception {
+        throw new Exception("Instantiate deny.");
+    }
+}
