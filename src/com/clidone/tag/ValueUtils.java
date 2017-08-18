4945f70df7e0120de3227f015e78ea556940ff20
huaxia0624 <huaxia0624@126.com>
2017-08-19 05:23:56 +0800

    add ValueUtils

---
+++ src/com/clidone/tag/ValueUtils.java
@@ -0,0 +1,48 @@
+package com.clidone.tag;
+
+/**
+ * <strong>Value Data Utils</strong>
+ * @author wuhuaxia
+ */
+public final class ValueUtils {
+
+    /**
+     * Validate value is empty or not
+     * @return true: empty, false: not empty
+     */
+    public final static boolean isEmpty(String value) {
+        return (value == null || "".equals(value.trim()));
+    }
+
+    /**
+     * Validate value is empty or not
+     * @return true: empty, false: not empty
+     */
+    public final static boolean isEmpty(Integer value) {
+        return (value == null);
+    }
+
+    /**
+     * Validate value is empty or not
+     * @return true: empty, false: not empty
+     */
+    public final static boolean isEmpty(Long value) {
+        return (value == null);
+    }
+
+    /**
+     * Validate value is empty or not
+     * @return true: empty, false: not empty
+     */
+    public final static boolean isEmpty(Double value) {
+        return (value == null);
+    }
+
+    /**
+     * Validate value is empty or not
+     * @return true: empty, false: not empty
+     */
+    public final static boolean isEmpty(Short value) {
+        return (value == null);
+    }
+}
