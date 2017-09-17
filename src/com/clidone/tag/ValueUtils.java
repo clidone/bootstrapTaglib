e90b1c5e939efd16c3256ba812e749ac4cf93680
huaxia0624 <huaxia0624@126.com>
2017-09-18 02:12:08 +0800

    Add 'get' method (By reflect)

--- src/com/clidone/tag/ValueUtils.java
+++ src/com/clidone/tag/ValueUtils.java
@@ -1,5 +1,7 @@
 package com.clidone.tag;
 
+import java.lang.reflect.Method;
+
 /**
  * <strong>Value Data Utils</strong>
  * @author wuhuaxia
@@ -52,5 +54,54 @@ public final class ValueUtils {
      */
     public final static boolean isEmpty(Short value) {
         return (value == null);
+    }
+
+    /**
+     * Get item data
+     * @param item data object
+     * @param key data key
+     * @return
+     */
+    public final static Object get(Object item, String key) {
+        if (item == null) {
+            return "";
+        }
+
+        Object value = null;
+
+        try {
+            Method[] methods = item.getClass().getMethods();
+            if (methods != null) {
+                String getter = buildGetter(key);
+                for (int i=0,len=methods.length; i<len; i++) {
+                    if (getter.equals(methods[i].getName())) {
+                        value = methods[i].invoke(item);
+                        break;
+                    }
+                }
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            value = "";
+        }
+
+        return value;
+    }
+
+    /**
+     * Build key getter method name
+     * @param key Key
+     * @return key getter method name
+     */
+    private final static String buildGetter(String key) {
+        if (isEmpty(key)) {
+            return "get";
+        }
+        String getter = String.valueOf(key.charAt(0)).toUpperCase();
+        if (key.length() > 1) {
+            getter += key.substring(1);
+        }
+        return "get" + getter;
     }
 }
