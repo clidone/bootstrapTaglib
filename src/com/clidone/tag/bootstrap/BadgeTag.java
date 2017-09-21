0614500bf9da79826c05859b36c39b35adaf654f
huaxia0624 <huaxia0624@126.com>
2017-09-22 04:13:12 +0800

    Add Badge tag

---
+++ src/com/clidone/tag/bootstrap/BadgeTag.java
@@ -0,0 +1,64 @@
+package com.clidone.tag.bootstrap;
+
+import javax.servlet.jsp.JspException;
+
+import com.clidone.tag.AbstractTag;
+import com.clidone.tag.ValueUtils;
+
+/**
+ * <strong>Badge tag</strong>
+ * @author wuhuaxia
+ */
+public class BadgeTag extends AbstractTag {
+
+    private static final long serialVersionUID = 971266254298297578L;
+
+    // **********************************************************************************
+    //
+    // Tag attributes
+    //
+    // **********************************************************************************
+    // text
+    protected String text = null;
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    // icon
+    protected String icon = null;
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    // **********************************************************************************
+    //
+    // Tag methods
+    //
+    // **********************************************************************************
+    /**
+     * @see AbstractTag#doEndTagV2()
+     */
+    @Override
+    protected String doEndTagV2() throws JspException {
+        setTagName("span");
+
+        addClass("badge");
+
+        if (!ValueUtils.isEmpty(icon)) {
+            String iconHTML = renderIcon(icon);
+            addBeforeContent(iconHTML);
+        }
+
+        addBeforeContent(text);
+
+        return render();
+    }
+
+    /**
+     * @see AbstractTag#doEndTagV3()
+     */
+    @Override
+    protected String doEndTagV3() throws JspException {
+        return doEndTagV2();
+    }
+}
