e421c181f7da1247ead964aa6ca92c129c02a9a8
huaxia0624 <huaxia0624@126.com>
2017-09-24 19:47:04 +0800

    Beta progress tag

--- src/com/clidone/tag/bootstrap/ProgressTag.java
+++ src/com/clidone/tag/bootstrap/ProgressTag.java
@@ -3,20 +3,62 @@ package com.clidone.tag.bootstrap;
 import javax.servlet.jsp.JspException;
 
 import com.clidone.tag.AbstractTag;
+import com.clidone.tag.ValueUtils;
 
 /**
- * <strong>Progress tag</strong>
+ * <strong>Progress Tag</strong>
  * @author wuhuaxia
  */
 public class ProgressTag extends AbstractTag {
 
-    private static final long serialVersionUID = -9051126136656993145L;
+    private static final long serialVersionUID = -8523132676940756577L;
 
     // **********************************************************************************
     //
     // Tag attributes
     //
     // **********************************************************************************
+    // value
+    protected double value = 0;
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    // minimum value
+    protected double minValue = 0;
+    public void setMin(double minValue) {
+        this.minValue = minValue;
+    }
+
+    // maximum value
+    protected double maxValue = 100;
+    public void setMax(double maxValue) {
+        this.maxValue = maxValue;
+    }
+
+    // theme
+    protected String theme = null;
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    // labelSrOnly
+    protected Boolean labelSrOnly = new Boolean(false);
+    public void setLabelSrOnly(Boolean labelSrOnly) {
+        this.labelSrOnly = labelSrOnly;
+    }
+
+    // striped
+    protected Boolean striped = null;
+    public void setStriped(Boolean striped) {
+        this.striped = striped;
+    }
+
+    // active
+    protected Boolean active = null;
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
 
     // **********************************************************************************
     //
@@ -28,6 +70,44 @@ public class ProgressTag extends AbstractTag {
      */
     @Override
     protected String doEndTagV2() throws JspException {
+        setTagName("div");
+
+        addBeforeWrap("<div class=\"progress\">");
+
+        addClass("progress-bar");
+        if (!ValueUtils.isEmpty(theme)) {
+            addClass("progress-bar-" + theme);
+        }
+        if (striped != null && striped.booleanValue()) {
+            addClass("progress-bar-striped");
+        }
+        if (active != null && active.booleanValue()) {
+            addClass("active");
+        }
+
+        double progress = Math.floor(((value - minValue) / (maxValue - minValue)) * 100);
+
+        boolean srOnly = (labelSrOnly != null && labelSrOnly.booleanValue());
+        if (srOnly) {
+            addBeforeContent(    "<span class=\"sr-only\">");
+        }
+        addBeforeContent(            progress + "%");
+        if (srOnly) {
+            addBeforeContent(    "</span>");
+        }
+        addBeforeContent("</div>");
+
+
+        addStyle("width",     String.valueOf(progress));
+        addStyle("min-width", "2em");
+
+        addAttribute("role",          "progressbar");
+        addAttribute("aria-valuenow", String.valueOf(value));
+        addAttribute("aria-valuemin", String.valueOf(minValue));
+        addAttribute("aria-valuemax", String.valueOf(maxValue));
+
+        addAfterWrap("</div>");
+
         return render();
     }
 
