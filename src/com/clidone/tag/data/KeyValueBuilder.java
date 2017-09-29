b0eebaada66645a1b62236c3cfc0bad3e3bd38a8
huaxia0624 <huaxia0624@126.com>
2017-09-29 11:42:22 +0800

    Add KeyValueBuilder

---
+++ src/com/clidone/tag/data/KeyValueBuilder.java
@@ -0,0 +1,62 @@
+package com.clidone.tag.data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <strong>KeyValue Builder</strong>
+ * @author wuhuaxia
+ */
+public class KeyValueBuilder {
+
+    /**
+     * Constructor
+     * @throws Exception
+     */
+    public KeyValueBuilder() throws Exception {
+        throw new Exception("Not support instantiate.");
+    }
+
+    /**
+     * Build List
+     * @param data key:value array
+     * @return key value list
+     */
+    public static List<KeyValue> build(String[][] data) {
+        if (data == null) {
+            return new ArrayList<KeyValue>();
+        }
+
+        int size  = data.length;
+        List<KeyValue> list = new ArrayList<KeyValue>(size);
+        for (int i=0; i<size; i++) {
+            list.add(new KeyValue(data[i][0], data[i][1]));
+        }
+
+        return list;
+    }
+
+    /**
+     * Build List
+     * @param dataList source data
+     * @param keyName data key name
+     * @param valueName data value name
+     * @return key value list
+     */
+    public static List<KeyValue> build(List<Map<?,?>> dataList, String keyName, String valueName) {
+        if (dataList == null) {
+            return new ArrayList<KeyValue>();
+        }
+
+        Map<?,?> data = null;
+        int  size = dataList.size();
+        List<KeyValue> list = new ArrayList<KeyValue>(size);
+        for (int i=0; i<size; i++) {
+            data = dataList.get(i);
+            list.add(new KeyValue(String.valueOf(data.get(keyName)), String.valueOf(data.get(valueName))));
+        }
+
+        return list;
+    }
+}
